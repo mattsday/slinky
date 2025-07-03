50958ae97081282bb1c937623833f0ce9308e3a6
Matt Day <mattsday@gmail.com>
2025-07-03 17:15:52 +0200

    fix: full control mapping

--- skyq.go
+++ skyq.go
@@ -2,54 +2,53 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"time"
 )
 
 // skyCommands maps the remote button names to their respective command codes.
 var skyCommands = map[string]int{
-	"power":        0,
-	"select":       1,
-	"return":       2,
-	"dismiss":      2,
-	"channelup":    6,
-	"channeldown":  7,
-	"interactive":  8,
-	"sidebar":      8,
-	"help":         9,
-	"services":     10,
-	"search":       10,
-	"menu":         11,
-	"home":         11,
-	"info":         14,
-	"text":         15,
-	"up":           16,
-	"down":         17,
-	"left":         18,
-	"right":        19,
-	"red":          32,
-	"green":        33,
-	"yellow":       34,
-	"blue":         35,
-	"0":            48,
-	"1":            49,
-	"2":            50,
-	"3":            51,
-	"4":            52,
-	"5":            53,
-	"6":            54,
-	"7":            55,
-	"8":            56,
-	"9":            57,
-	"play":         64,
-	"pause":        65,
-	"stop":         66,
-	"record":       67,
-	"fast-forward": 69,
-	"rewind":       71,
-	"boxoffice":    240,
-	"sky":          241,
+	"power":           0,
+	"select":          1,
+	"return":          2,
+	"dismiss":         2,
+	"channelup":       6,
+	"channeldown":     7,
+	"interactive":     8,
+	"sidebar":         8,
+	"help":            9,
+	"services":        10,
+	"search":          10,
+	"menu":            11,
+	"home":            11,
+	"info":            14,
+	"text":            15,
+	"direction-up":    16,
+	"direction-down":  17,
+	"direction-left":  18,
+	"direction-right": 19,
+	"red":             32,
+	"green":           33,
+	"yellow":          34,
+	"blue":            35,
+	"0":               48,
+	"1":               49,
+	"2":               50,
+	"3":               51,
+	"4":               52,
+	"5":               53,
+	"6":               54,
+	"7":               55,
+	"8":               56,
+	"9":               57,
+	"play":            64,
+	"pause":           65,
+	"stop":            66,
+	"record":          67,
+	"fast-forward":    69,
+	"rewind":          71,
+	"boxoffice":       240,
+	"sky":             241,
 }
 
 // sendSkyCommand connects to the Sky Q box and sends a single command.
@@ -58,8 +57,6 @@ func sendSkyCommand(host string, port int, command string) error {
 	if !ok {
 		return fmt.Errorf("unknown sky command: %s", command)
 	}
-
-	fmt.Printf("Sending Sky Q Command to %v:%v -- %v\n", host, port, command)
 
 	// 1. Establish a TCP connection with a timeout.
 	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", host, port), 2*time.Second)
@@ -111,6 +108,5 @@ func sendSkyCommand(host string, port int, command string) error {
 		}
 	}
 
-	log.Printf("Sent Sky Q command: %s", command)
 	return nil
 }
